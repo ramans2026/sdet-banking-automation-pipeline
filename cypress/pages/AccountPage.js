12a78088fffa1345157eb1b3fd13dfec4b38eeb2
ramans2026 <djramans1985@gmail.com>
2026-02-11 22:07:52 +0530

    Fix detached element issue in withdraw for CI

--- cypress/pages/AccountPage.js
+++ cypress/pages/AccountPage.js
@@ -11,9 +11,12 @@ class AccountPage {
       .should('be.visible')
       .click();
 
+    // wait for form to stabilize
     cy.get('input[ng-model="amount"]', { timeout: 10000 })
       .should('be.visible')
-      .and('not.be.disabled')
+      .and('not.be.disabled');
+
+    cy.get('input[ng-model="amount"]')
       .clear()
       .type(amount);
 
@@ -27,9 +30,13 @@ class AccountPage {
       .should('be.visible')
       .click();
 
+    // wait for form to stabilize
     cy.get('input[ng-model="amount"]', { timeout: 10000 })
       .should('be.visible')
-      .and('not.be.disabled')
+      .and('not.be.disabled');
+
+    // re-query the element before typing
+    cy.get('input[ng-model="amount"]')
       .clear()
       .type(amount);
 
