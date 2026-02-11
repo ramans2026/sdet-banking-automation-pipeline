1b7970be5cd459bfd5aca0b4a6b8f0d631feea46
ramans2026 <djramans1985@gmail.com>
2026-02-11 22:00:18 +0530

    Fix cross-browser CI using Cypress official action

--- cypress/pages/AccountPage.js
+++ cypress/pages/AccountPage.js
@@ -1,20 +1,45 @@
 class AccountPage {
+  waitForAccountPage() {
+    cy.contains('Account Number', { timeout: 15000 })
+      .should('be.visible');
+  }
+
   deposit(amount) {
-    cy.contains('Deposit').click();
-    cy.get('input[ng-model="amount"]').clear().type(amount);
+    this.waitForAccountPage();
+
+    cy.contains('Deposit', { timeout: 10000 })
+      .should('be.visible')
+      .click();
+
+    cy.get('input[ng-model="amount"]', { timeout: 10000 })
+      .should('be.visible')
+      .and('not.be.disabled')
+      .clear()
+      .type(amount);
+
     cy.get('button[type="submit"]').click();
   }
 
   withdraw(amount) {
-    cy.contains('Withdrawl').click();
-    cy.get('input[ng-model="amount"]').clear().type(amount);
+    this.waitForAccountPage();
+
+    cy.contains('Withdrawl', { timeout: 10000 })
+      .should('be.visible')
+      .click();
+
+    cy.get('input[ng-model="amount"]', { timeout: 10000 })
+      .should('be.visible')
+      .and('not.be.disabled')
+      .clear()
+      .type(amount);
+
     cy.get('button[type="submit"]').click();
   }
 
-verifySuccess() {
-  cy.contains(/successful/i, { timeout: 10000 })
-    .should('be.visible');
-}
+  verifySuccess() {
+    cy.contains(/successful/i, { timeout: 10000 })
+      .should('be.visible');
+  }
 }
 
 export default new AccountPage();
--- cypress/pages/LoginPage.js
+++ cypress/pages/LoginPage.js
@@ -1,19 +1,26 @@
 class LoginPage {
   visit() {
     cy.visit('/');
+    cy.contains('Customer Login', { timeout: 15000 })
+      .should('be.visible');
   }
 
   openCustomerLogin() {
-    cy.contains('Customer Login', { timeout: 10000 }).click();
+    cy.contains('Customer Login', { timeout: 15000 })
+      .should('be.visible')
+      .click();
   }
 
   selectUser(username) {
-    cy.get('#userSelect', { timeout: 10000 }).should('be.visible');
+    cy.get('#userSelect', { timeout: 15000 })
+      .should('be.visible');
     cy.get('#userSelect').select(username);
   }
 
   clickLogin() {
-    cy.contains('Login').click();
+    cy.contains('Login', { timeout: 10000 })
+      .should('be.visible')
+      .click();
   }
 }
 
